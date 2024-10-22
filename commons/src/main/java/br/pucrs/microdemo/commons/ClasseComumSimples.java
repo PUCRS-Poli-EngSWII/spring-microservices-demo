631639188fbc8a21be83a2e7eb8d52dc9100b227
Michael Móra <michael.c.mora@gmail.com>
2024-10-21 21:50:37 -0300

    Atualizações pontuais - rodando com Compose

--- commons/src/main/java/br/pucrs/microdemo/commons/ClasseComumSimples.java
+++ commons/src/main/java/br/pucrs/microdemo/commons/ClasseComumSimples.java
@@ -5,10 +5,10 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Setters
-@Getters
-@AlllArgsConstructor
-@NoArgsConstructors
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class ClasseComumSimples {
     private String texto;
 }
